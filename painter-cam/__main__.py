a044dd88fa8446e7bc716277040e7f661468b89a
Rob Chiocchio <rmchiocchio@wpi.edu>
2020-11-02 07:26:59 -0500

    Working with blackstripes

--- painter-cam/__main__.py
+++ painter-cam/__main__.py
@@ -1,9 +1,77 @@
+import os
 import argparse
 import cv2 as cv
 import numpy as np
 
+from blackstripes import spiral, sketchy, crossed
+
+# Blackstripes
+LEVELS = [180, 108, 180, 108]
+LINE_WIDTH = 2.0
+LINE_SPACING = 2
+LINE_COLOR = "#000000"
+MAX_LINE_LENGTH = 100
+INTERNAL_LINE_SIZE = 2
+SCALE = 1.0
+ROUND = False
+
 # DEFAULT PARAMETERS
 DEF_COLORS = 8
+
+def getOutput(input_path, method=""):
+    normpath = os.path.normpath(input_path)
+    name = ".".join(normpath.split(".")[:-1])
+    if len(method) > 0:
+        name += f"-{method}"
+    print(name)
+    return name + ".svg"
+
+
+def getOutputPngPath(output_path):
+    return output_path + ".png"
+
+def applySpiral():
+    draw_args = [
+        args.filename,
+        getOutput(args.filename, method="spiral"),
+        LINE_WIDTH,
+        LINE_COLOR,
+        SCALE
+    ] \
+        + LEVELS \
+        + [LINE_SPACING] \
+        + [0,0,0] \
+        + [ROUND]
+    spiral.draw(*draw_args)
+
+
+def applyCrossed():
+    draw_args = [
+        args.filename,
+        getOutput(args.filename, method="crossed"),
+        LINE_WIDTH,
+        LINE_COLOR,
+        SCALE
+    ] \
+        + LEVELS \
+        + [1] \
+        + [0,0,0]
+    crossed.draw(*draw_args)
+
+
+def applySketchy():
+    draw_args = [
+        args.filename,
+        getOutput(args.filename, method="sketchy"),
+        LINE_WIDTH,
+        MAX_LINE_LENGTH,
+        LINE_COLOR,
+        SCALE,
+        INTERNAL_LINE_SIZE,
+    ] \
+        + [0,0,0]
+    sketchy.draw(*draw_args)
+
 
 def processImage():
     img_raw = cv.imread(args.filename) # load image
@@ -43,18 +111,18 @@ def processImage():
 
     img_contours = np.array(img)
     contours = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) # detect contours
-    cv.drawContours(img_contours, contours[0], -1, (0,255,0), 1)
+    #cv.drawContours(img_contours, contours[0], -1, (0,255,0), 1)
 
     #mask_blue = cv.inRange(hsv, np.array([55, 0, 0]), np.array([120, 255, 255]))
     #blue = cv.bitwise_and(img, img, mask=mask_blue)
     
     # DEBUG: show image
-    cv.imshow("image", np.hstack([img_raw, img, hsv, img_contours]))
-    cv.imshow("image2", np.hstack([hue, saturation, value, edges]))
-    while cv.getWindowProperty("image", cv.WND_PROP_VISIBLE) == 1:
-        if cv.waitKey(100) >= 0:
-            break
-    cv.destroyAllWindows()
+    #cv.imshow("image", np.hstack([img_raw, img, hsv, img_contours]))
+    #cv.imshow("image2", np.hstack([hue, saturation, value, edges]))
+    #while cv.getWindowProperty("image", cv.WND_PROP_VISIBLE) == 1:
+    #    if cv.waitKey(100) >= 0:
+    #        break
+    #cv.destroyAllWindows()
 
     return img
     #return hsv
@@ -67,7 +135,10 @@ def main():
     global args
     args = parser.parse_args()
 
-    img = processImage()
+    #img = processImage()
+    applySpiral()
+    applyCrossed()
+    applySketchy()
 
 if __name__ == "__main__":
     main()
