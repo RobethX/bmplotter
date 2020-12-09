91314b666ccb2261d64d0afc7b686acf2030d23b
Rob Chiocchio <rmchiocchio@wpi.edu>
2020-12-09 02:13:23 -0500

    Cleanup and some refactoring

--- painter-cam/__main__.py
+++ painter-cam/__main__.py
@@ -3,7 +3,6 @@ import argparse
 import logging
 import cv2 as cv
 import numpy as np
-#from matplotlib import pyplot as plt
 
 from svgpathtools import svg2paths, wsvg
 
@@ -40,65 +39,22 @@ def processImage():
     img = cv.bilateralFilter(img, 9, 50, 50) # bilateral filter
     #img = cv.ximgproc.anisotropicDiffusion(img, 0.5, 0.02, 10) # 2d anisotropic diffusion
 
-    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV) # convert image to HSV
-    hue = hsv[:,:,0] # get hue channel
-    saturation = hsv[:,:,1] # get saturation channel
-    value = hsv[:,:,2] # get value channel (brightness)
-
-    edges_hue = cv.Canny(hue, 40, 60) # detect edges
-    edges_saturation = cv.Canny(saturation, 40, 60) # detect edges
-    edges_value = cv.Canny(value, 40, 60) # detect edges
-    edges_hsv = edges_hue+edges_saturation+edges_value
-
-    #res = 256 // args.colors
-
-    #hue = (hue // 32) * 32 # quantize hue
-
-    #saturation = cv.normalize(saturation, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX)
-    #saturation = (saturation // 32) * 32
-    #saturation = cv.adaptiveThreshold(saturation, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
-    #saturation = cv.bilateralFilter(saturation, 9, 75, 75) # bilateral filter
-
-    #value = cv.normalize(value, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX)
-    #value = (value // 32) * 32
-
-    #hsv = [hue, saturation, value]
-    hsv[:,:,0] = hue
-    hsv[:,:,1] = saturation
-    hsv[:,:,2] = value
-
-    img = cv.cvtColor(hsv, cv.COLOR_HSV2BGR) # convert back to RGB
-
-    edges = cv.Canny(cv.medianBlur(img,3), 40, 60) # detect edges
-
-    img_contours = np.array(img)
-    contours = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) # detect contours
-    cv.drawContours(img_contours, contours[0], -1, (0,255,0), 1)
-
-    thresh = cv.adaptiveThreshold(value, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
-    thresh = cv.erode(thresh, None, iterations=3)
-
-    #mask_blue = cv.inRange(hsv, np.array([55, 0, 0]), np.array([120, 255, 255]))
-    #blue = cv.bitwise_and(img, img, mask=mask_blue)
-    
     # DEBUG: show image
-    #cv.imshow("image", np.hstack([img_raw, img, hsv, img_contours]))
-    #cv.imshow("image2", np.hstack([hue, saturation, value, edges]))
-    cv.imshow("image", np.vstack([np.hstack([hue, saturation, value, edges]), np.hstack([thresh, edges_saturation, edges_value, edges_hsv])]))
+    cv.imshow("image", np.hstack([img_raw, img]))
     while cv.getWindowProperty("image", cv.WND_PROP_VISIBLE) == 1:
         if cv.waitKey(100) >= 0:
             break
     cv.destroyAllWindows()
 
     return img
-    #return hsv
 
 def main():
     parser = argparse.ArgumentParser(prog="painter-cam")
     parser.add_argument("filename", help="input image to convert")
     parser.add_argument("-c", help="amount of paint colors", action="store", dest="colors", default=DEF_NUM_COLORS, type=int)
-    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true", dest="verbose")
     parser.add_argument("--log-file", help="log file path", action="store", dest="log_file", default=None, type=str)
+    parser.add_argument("--no-black", help="skip black layer", action="store_false", dest="black_layer")
 
     global args
     args = parser.parse_args()
@@ -113,8 +69,6 @@ def main():
     #img = processImage()
     img = cv.imread(args.filename)
 
-    #img = utils.image.removeWhite(img)
-
     tmp = utils.path.TempFolder()
 
     tmp_path = os.path.normpath("tmp")
@@ -122,34 +76,15 @@ def main():
     if not os.path.exists(tmp_path):
         os.mkdir(tmp_path)
 
-    #cmyk = utils.color.BGR2CMYK(img)
-    #cmy = utils.color.BGR2CMY(img)
-
     channels = cv.split(img) #np.mod(cmy, 255)
-    #channels = cv.split(cmyk)
-    channels.append(cv.cvtColor(img, cv.COLOR_BGR2GRAY))
+    if args.black_layer:
+        channels.append(cv.cvtColor(img, cv.COLOR_BGR2GRAY))
 
     c = 0
     while c < len(channels):
         log.info(f"Vectorizing layer {c}...")
         path = os.path.join(tmp_path, f"img-{c}.png")
-        _, labels, centers = cv.kmeans(np.float32(channels[c]), KMEANS_ITERATIONS, None, KMEANS_CRITERIA, KMEANS_ATTEMPTS, KMEANS_FLAGS)
-        #centers = (centers // 8) * 8
-        centers = np.uint8(centers)
         cv.imwrite(path, channels[c])
-
-        levels = []
-
-        for l in np.unique(labels): # TODO: rewrite better
-            x = []
-            i = 0
-            while i < len(labels):
-                if labels[i] == l:
-                    x.append(centers[l,i])
-                i += 1
-            avg = np.uint8(np.mean(x))
-            levels.append(avg)
-
         output_path = os.path.join(tmp_path, f"img-{c}")
         #output_path = tmp.getPath(f"img-{c}")
         gen = generators.Squiggle(path, f"{output_path}.svg")
--- painter-cam/utils/__init__.py
+++ painter-cam/utils/__init__.py
@@ -1,4 +1,2 @@
-from .color import *
-from .image import *
 from .path import *
 from .svg import *
